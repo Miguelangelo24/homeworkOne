f2aad2dbc8a406ea891a1661e0da7db6564bf6a5
Miguelangelo Martinez <michaelangelo.m98@gmail.com>
2018-04-03 15:11:26 -0700

    Initial commit

---
+++ homeworkOne.c
@@ -0,0 +1,18 @@
+/*
+ * hwOne1.c
+ *
+ *  Created on: Apr 2, 2018
+ *      Author: Miguelangelo Martinez
+ */
+#include <stdio.h>
+
+//First Homework Program
+int main() {
+	/* My first HW
+	   program in
+	   C
+	 */
+	printf("Hello World\n");
+
+	return 0;
+}
